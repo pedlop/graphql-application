aa4d90477f99d0eec5724cca9c541ea49c691ddc
Pedro Victor <pedrolopes09.pv@gmail.com>
2019-04-16 15:19:25 -0300

    Updates

---
+++ precache-manifest.0f391d72914afb0dcf29e51b8e5fde92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccea3093d0134b6c6f73",
+    "url": "/graphql-application/static/css/main.0046d956.chunk.css"
+  },
+  {
+    "revision": "ccea3093d0134b6c6f73",
+    "url": "/graphql-application/static/js/main.2be85f70.chunk.js"
+  },
+  {
+    "revision": "474f575b8ed50eec8a66",
+    "url": "/graphql-application/static/js/runtime~main.36ded767.js"
+  },
+  {
+    "revision": "4e2f1a589b0bd011de58",
+    "url": "/graphql-application/static/js/2.be63daba.chunk.js"
+  },
+  {
+    "revision": "69f031114db81e5e7a8c4e1b8c87278d",
+    "url": "/graphql-application/index.html"
+  }
+];
